c6c5e378e7cb0286bddb96a23b8c42cfb9d1e0c7
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2024-12-16 16:43:55 +0900

    fine tuning

--- models/models.py
+++ models/models.py
@@ -1,4 +1,13 @@
+import os 
+import torch
 from .detector.yolox import YOLOXDetector
 
 def get_detector(model_cfg):
-    return YOLOXDetector(model_cfg)
+    model = YOLOXDetector(model_cfg)
+
+    if model_cfg.weight is not None:
+        ckpt_path = os.path.join('./config/model/', model_cfg.weight)
+        ckpt = torch.load(ckpt_path)
+        model.load_weights(ckpt['model'])
+
+    return model
