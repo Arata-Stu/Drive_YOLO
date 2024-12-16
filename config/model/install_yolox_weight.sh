26711c57dc732309a36e7adc77c222db16957760
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2024-12-16 16:08:48 +0900

    add weight

---
+++ config/model/install_yolox_weight.sh
@@ -0,0 +1,4 @@
+wget https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_x.pth
+wget https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_l.pth
+wget https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_m.pth
+wget https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_s.pth
