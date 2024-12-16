7dea6e394bd51a669583b3a2312f1f7f126446bc
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2024-12-16 18:42:00 +0900

    fix

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from .detector.yolox import YOLOXDetector
 def get_detector(model_cfg):
     model = YOLOXDetector(model_cfg)
 
-    if model_cfg.weight is not None:
+    if model_cfg.pretrained:
         ckpt_path = os.path.join('./config/model/', model_cfg.weight)
         ckpt = torch.load(ckpt_path)
         model.load_weights(ckpt['model'])
