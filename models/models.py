c9c0976018cc70737841bc36df1ea5bf8c913c9b
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2024-12-17 17:45:08 +0900

    fix model load weight

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import torch
 from .detector.yolox import YOLOXDetector
 
@@ -6,8 +6,18 @@ def get_detector(model_cfg):
     model = YOLOXDetector(model_cfg)
 
     if model_cfg.pretrained:
+        # チェックポイントファイルのパスを取得
         ckpt_path = os.path.join('./config/model/', model_cfg.weight)
         ckpt = torch.load(ckpt_path)
-        model.load_state_dict(ckpt['model'])
-
+        
+        # ヘッド部分のキーを除外
+        model_state_dict = ckpt['model']
+        backbone_neck_state_dict = {
+            k: v for k, v in model_state_dict.items() 
+            if not k.startswith('head')  # head部分のキーをフィルタリング
+        }
+        
+        # 残った部分だけを読み込む
+        model.load_state_dict(backbone_neck_state_dict, strict=False)
+    
     return model
